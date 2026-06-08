2fe5d360dcee0c3668fb811e35201c59e1ba0d74
SANECA MACK <SANECATV@GMAIL.COM>
2026-06-08 17:19:51 -0400

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  experimental: {
-    cacheComponents: true,
-  },
+  cacheComponents: true,
 
   images: {
     remotePatterns: [
