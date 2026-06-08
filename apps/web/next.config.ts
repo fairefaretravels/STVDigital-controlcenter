3da7be8332c32603b277a604c376e1c019b8864a
SANECA MACK <SANECATV@GMAIL.COM>
2026-06-08 17:11:11 -0400

    Add experimental cacheComponents option to next.config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
+  experimental: {
+    cacheComponents: true,
+  },
+
   images: {
     remotePatterns: [
       {
